0feac4382d2a75655b70e99a0f6a74f00631960d
Derek <derekb0147@gmail.com>
2020-09-06 18:39:02 -0400

    Clear text from the screen

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use termion::color;
+use termion::clear;
 
 fn main() {
     println!("{red}Hello, world!{reset}",
         red     = color::Fg(color::Red),
         reset   = color::Fg(color::Reset)
     );
+
+    /*
+     * clear::All
+     * clear::CurrentLine
+     * clear::AfterCursor
+     * clear::BeforeCursor
+     * See the docs: https://docs.rs/termion/1.5.5/termion/
+     */
+    println!("{}", clear::All);
 }
