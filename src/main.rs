e30b89c39e0a3c15c5e290a440ba56da7b391f03
Derek <derekb0147@gmail.com>
2020-09-06 18:58:39 -0400

    Cursor magic

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-use termion::color;
-use termion::clear;
+use termion::{color, clear, cursor};
+
+// Work out how that grid works
 
 fn main() {
-    println!("{red}Hello, world!{reset}",
-        red     = color::Fg(color::Red),
-        reset   = color::Fg(color::Reset)
-    );
+    println!("{clear}{goto}{red}more red than any comrade{reset}",
+         // Full screen clear.
+         clear = clear::All,
+         // Goto the cell.
+         goto  = cursor::Goto(4, 2),
+         red   = color::Fg(color::Red),
+         reset = color::Fg(color::Reset));
 
-    /*
-     * clear::All
-     * clear::CurrentLine
-     * clear::AfterCursor
-     * clear::BeforeCursor
-     * See the docs: https://docs.rs/termion/1.5.5/termion/
-     */
-    println!("{}", clear::All);
+
+    println!("{goto}{red}more red than any comrade{reset}",
+             goto  = cursor::Goto(6, 6),
+             red   = color::Fg(color::Red),
+             reset = color::Fg(color::Reset));
+
+
+    // Clear the line and print some new stuff
+    println!("Otherwise!{goto}{magenta}magenta{blue}blue{green}green{red}communism{reset}",
+            goto    = cursor::Goto(8, 3),
+            magenta = color::Fg(color::Magenta),
+            red     = color::Fg(color::Red),
+            blue    = color::Fg(color::Blue),
+            green   = color::Fg(color::Green),
+            reset   = color::Fg(color::Reset));
 }
