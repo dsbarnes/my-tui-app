af7fca023b03fc1fbe96de2f5c16ad39187dd9f1
Derek <derekb0147@gmail.com>
2020-09-06 19:06:18 -0400

    style

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,11 @@
-use termion::{color, clear, cursor};
-
-// Work out how that grid works
+use termion::{color, style};
 
 fn main() {
-    println!("{clear}{goto}{red}more red than any comrade{reset}",
-         // Full screen clear.
-         clear = clear::All,
-         // Goto the cell.
-         goto  = cursor::Goto(4, 2),
-         red   = color::Fg(color::Red),
-         reset = color::Fg(color::Reset));
-
-
-    println!("{goto}{red}more red than any comrade{reset}",
-             goto  = cursor::Goto(6, 6),
-             red   = color::Fg(color::Red),
-             reset = color::Fg(color::Reset));
-
-
-    // Clear the line and print some new stuff
-    println!("Otherwise!{goto}{magenta}magenta{blue}blue{green}green{red}communism{reset}",
-            goto    = cursor::Goto(8, 3),
+    println!("{magenta}Magenta{blue}blue{green}Green{red}{bold}communism{reset}",
+            bold  = style::Bold,
             magenta = color::Fg(color::Magenta),
-            red     = color::Fg(color::Red),
-            blue    = color::Fg(color::Blue),
-            green   = color::Fg(color::Green),
-            reset   = color::Fg(color::Reset));
+            red   = color::Fg(color::Red),
+            blue  = color::Fg(color::Blue),
+            green = color::Fg(color::Green),
+            reset = style::Reset);
 }
